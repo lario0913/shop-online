2f69a0cc869ca268763702382dad0937d60318fe
Lario <osunkunleolanrewaju@gmail.com>
2019-12-30 13:04:28 +0100

    finish sign in handle submit method

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -4,7 +4,7 @@ import './sign-in.styles.scss'
 import FormInput from '../form-input/form-input.component'
 import CustomButton from '../custom-button/custom-button.component'
 // import { signInWithGoogle }  from '../../firebase/firebase.utils'
-import { signInWithGoogle } from '../../firebase/firebase.utils'
+import {auth, signInWithGoogle } from '../../firebase/firebase.utils'
 
 
 class SignIn extends React.Component {
@@ -18,14 +18,19 @@ class SignIn extends React.Component {
         }
     }
 
-    handleSubmit= (event) => {
+    handleSubmit= async event => {
         event.preventDefault();
 
+        const {email, password} = this.state
 
-        this.setState({
-            email: '',
-            password: ''
-        })
+        try{
+            await auth.signInWithEmailAndPassword(email, password)
+            this.setState({ email: '', password: '' })  
+        }catch(error){
+            console.log(error);
+        }
+
+        
     }
 
     handleChange = (event) => {
