c11188beb13178e3e13bc7514485eb3cd9e8e948
Lario <osunkunleolanrewaju@gmail.com>
2019-12-30 12:46:53 +0100

    implemented firebase utils, included ability to store authenticated users into firebase and implemeted sign up with email and password

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import {auth} from './firebase/firebase.utils'
+import {auth, createUserProfileDocument} from './firebase/firebase.utils'
 
 
 class App extends React.Component {
@@ -22,8 +22,25 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user=> {
-      this.setState({currentUser:user})
+    this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth=> {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument (userAuth)
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser : {
+              id: snapShot,
+              ...snapShot.data()
+            }
+          })
+
+          console.log(this.state)
+        })
+
+      }
+      else {
+        this.setState({currentUser: userAuth})
+      }
     })
   }
   
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 import './sign-in-and-sign-up.styles.scss'
 import SignIn from '../../components/sign-in/sign-in.component'
+import SignUp from '../../components/sign-up/sign-up.component'
 
 export const SignInAndSignUpPage = () => (
-    <div className='sign-in-and-out'>
+    <div className='sign-in-and-up'>
         <SignIn />
+        <SignUp />
     </div>
 )
 
