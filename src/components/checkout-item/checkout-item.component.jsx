eeff92883887842cdd6848091d10af3d5145a76d
Lario <osunkunleolanrewaju@gmail.com>
2020-01-06 14:02:50 +0100

    added addItem and removeItem functionality to the checkout item component

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -2,16 +2,33 @@ import React from 'react'
 
 import './checkout-item.styles.scss'
 
-const CheckoutItem = ({cartItem: {name, imageUrl, quantity, price}}) => (
-    <div className='checkout-item'>
-        <div className='image-container'>
-            <img src={imageUrl} alt="item"/>
-        </div>
-        <span className='name'>{name}</span>
-        <span className='quantity'>{quantity}</span>
-        <span className='price'>{price}</span>
-        <div className='remove-button'>&#10005;</div>
-    </div>
-)
+import {connect} from 'react-redux'
 
-export default CheckoutItem
+import {clearItemFromCart, removeItem, addItem} from '../../redux/cart/cart.action'
+
+const CheckoutItem = ({cartItem, clearItem, addItem, removeItem}) => {
+    const {name, imageUrl, quantity, price} = cartItem
+
+    return(
+        <div className='checkout-item'>
+            <div className='image-container'>
+                <img src={imageUrl} alt="item"/>
+            </div>
+            <span className='name'>{name}</span>
+            <span className='quantity'>
+                <div className='arrow' onClick={()=>removeItem(cartItem)}>&#10094;</div>
+                <span className='value'>{quantity}</span> 
+                <div className='arrow' onClick={()=>addItem(cartItem)}>&#10095;</div>
+            </span>
+            <span className='price'>{price}</span>
+            <div className='remove-button' onClick={()=>clearItem(cartItem)}>&#10005;</div>
+        </div>
+    )}
+
+const mapDispatchToProps = dispatch => ({
+    clearItem: item => dispatch(clearItemFromCart(item)),
+    addItem : item => dispatch(addItem(item)),
+    removeItem : item => dispatch(removeItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CheckoutItem)
